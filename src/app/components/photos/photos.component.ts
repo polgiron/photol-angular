8ff4f1140ef784537b323826b5563974e4ad9d7e
Paul Giron <pol.giron@gmail.com>
2019-04-25 10:25:25 +0100

    Add js maconry

--- src/app/components/photos/photos.component.ts
+++ src/app/components/photos/photos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import Macy from 'Macy';
 
 @Component({
   selector: 'app-photos',
@@ -7,33 +8,50 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PhotosComponent implements OnInit {
   @Input() photos: any;
-  numberOfColumns: number = 3;
+  // numberOfColumns: number = 3;
 
   constructor() { }
 
   ngOnInit() {
-    this.reorder(this.photos);
-  }
+    // this.reorder(this.photos);
 
-  reorder(array) {
-    array.forEach((element, index) => {
-      element.index = index;
-    });
-
-    const out = [];
-    let col = 0;
-
-    while (col < this.numberOfColumns) {
-      for (let i = 0; i < array.length; i += this.numberOfColumns) {
-        let _val = array[i + col];
-        if (_val !== undefined) {
-          out.push(_val);
+    const macyInstance = Macy({
+      container: '#photos-list',
+      columns: 3,
+      trueOrder: true,
+      margin: 16,
+      breakAt: {
+        992: {
+          margin: 16,
+          columns: 2
+        },
+        767: {
+          margin: 16,
+          columns: 1
         }
       }
-      col++;
-    }
-
-    console.log(out);
-    this.photos = out;
+    });
   }
+
+  // reorder(array) {
+  //   array.forEach((element, index) => {
+  //     element.index = index;
+  //   });
+
+  //   const out = [];
+  //   let col = 0;
+
+  //   while (col < this.numberOfColumns) {
+  //     for (let i = 0; i < array.length; i += this.numberOfColumns) {
+  //       let _val = array[i + col];
+  //       if (_val !== undefined) {
+  //         out.push(_val);
+  //       }
+  //     }
+  //     col++;
+  //   }
+
+  //   console.log(out);
+  //   this.photos = out;
+  // }
 }
