c2223166ca617d647caa9af0dcd02203da6e0667
Paul Giron <pol.giron@gmail.com>
2019-04-26 14:02:38 +0100

    Disable Macy...

--- src/app/components/photos/photos.component.ts
+++ src/app/components/photos/photos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PhotoService } from 'src/app/services/photo.service';
-import Macy from 'Macy';
+// import Macy from 'Macy';
 
 @Component({
   selector: 'app-photos',
@@ -9,7 +9,7 @@ import Macy from 'Macy';
 })
 export class PhotosComponent implements OnInit {
   @Input() photos: any;
-  // numberOfColumns: number = 3;
+  numberOfColumns: number = 3;
 
   constructor(
     private photoService: PhotoService
@@ -17,45 +17,44 @@ export class PhotosComponent implements OnInit {
 
   ngOnInit() {
     this.photoService.currentPhotos = this.photos;
-    // this.reorder(this.photos);
+    this.reorder(this.photos);
 
-    const macyInstance = Macy({
-      container: '#photos-list',
-      columns: 3,
-      trueOrder: true,
-      margin: 16,
-      breakAt: {
-        992: {
-          margin: 16,
-          columns: 2
-        },
-        767: {
-          margin: 16,
-          columns: 1
-        }
-      }
-    });
+    // const macyInstance = Macy({
+    //   container: '#photos-list',
+    //   columns: 3,
+    //   trueOrder: true,
+    //   margin: 16,
+    //   breakAt: {
+    //     992: {
+    //       margin: 16,
+    //       columns: 2
+    //     },
+    //     767: {
+    //       margin: 16,
+    //       columns: 1
+    //     }
+    //   }
+    // });
   }
 
-  // reorder(array) {
-  //   array.forEach((element, index) => {
-  //     element.index = index;
-  //   });
+  reorder(array) {
+    array.forEach((element, index) => {
+      element.index = index;
+    });
 
-  //   const out = [];
-  //   let col = 0;
+    const out = [];
+    let col = 0;
 
-  //   while (col < this.numberOfColumns) {
-  //     for (let i = 0; i < array.length; i += this.numberOfColumns) {
-  //       let _val = array[i + col];
-  //       if (_val !== undefined) {
-  //         out.push(_val);
-  //       }
-  //     }
-  //     col++;
-  //   }
+    while (col < this.numberOfColumns) {
+      for (let i = 0; i < array.length; i += this.numberOfColumns) {
+        let _val = array[i + col];
+        if (_val !== undefined) {
+          out.push(_val);
+        }
+      }
+      col++;
+    }
 
-  //   console.log(out);
-  //   this.photos = out;
-  // }
+    this.photos = out;
+  }
 }
